14422ce16bf9e1a551e0b8c41da60a9a1364ac8a
rezatafreshi80 <163215865+rezatafreshi80@users.noreply.github.com>
2026-07-20 00:36:35 +0330

    Update tafreshiirancid_logic.sh

--- tafreshiirancid_logic.sh
+++ tafreshiirancid_logic.sh
@@ -1,27 +1,73 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-CONFIG_FILE="/etc/asterisk/tafreshi_cid.conf"
+CONFIG_FILE="/etc/asterisk/tafreshiirancid.conf"
 
-# Read province code from config file
-if [ -f "$CONFIG_FILE" ]; then
-    PROVINCE_CODE="$(cat "$CONFIG_FILE" | tr -d '[:space:]')"
-else
-    PROVINCE_CODE="021"
-fi
+read_config() {
+    if [[ -f "$CONFIG_FILE" ]]; then
+        PROVINCE_CODE="$(grep -E '^PROVINCE_CODE=' "$CONFIG_FILE" | head -n1 | cut -d= -f2- | tr -d '[:space:]')"
+    fi
 
-# Asterisk passes CallerID as the first argument
-RAW_CID="$1"
+    [[ -z "$PROVINCE_CODE" ]] && PROVINCE_CODE="021"
+}
 
-# Keep digits only
-CID="$(echo "$RAW_CID" | sed 's/[^0-9]//g')"
+agi_read_env() {
+    while IFS= read -r line; do
+        [[ -z "$line" ]] && break
+    done
+}
 
-FINAL_CID="$CID"
+agi_cmd() {
+    local cmd="$1"
+    local reply
 
-# If it's an 8-digit local number, prefix province code
-if [[ "$CID" =~ ^[1-9][0-9]{7}$ ]]; then
-    FINAL_CID="${PROVINCE_CODE}${CID}"
-fi
+    printf '%s\n' "$cmd"
+    IFS= read -r reply || return 1
+    printf '%s\n' "$reply"
+}
 
-# Send result back to Asterisk AGI
-echo "SET VARIABLE CALLERID(num) ${FINAL_CID}"
-exit 0
+get_agi_variable() {
+    local varname="$1"
+    local reply value
+
+    reply="$(agi_cmd "GET VARIABLE ${varname}")" || return 1
+    value="$(awk -F'[()]' '/^200 result=1/ {print $2}' <<< "$reply")"
+    printf '%s' "$value"
+}
+
+set_agi_variable() {
+    local varname="$1"
+    local value="$2"
+
+    agi_cmd "SET VARIABLE ${varname} ${value}" >/dev/null
+}
+
+normalize_callerid() {
+    local raw_cid digits final_cid
+
+    raw_cid="$(get_agi_variable "CALLERID(num)")"
+    digits="$(printf '%s' "$raw_cid" | tr -cd '0-9')"
+
+    final_cid="$digits"
+
+    # If it looks like a local number, prefix province code
+    if [[ "$digits" =~ ^[1-9][0-9]{7}$ ]]; then
+        final_cid="${PROVINCE_CODE}${digits}"
+    fi
+
+    printf '%s' "$final_cid"
+}
+
+main() {
+    read_config
+    agi_read_env
+
+    final_cid="$(normalize_callerid)"
+
+    if [[ -n "$final_cid" ]]; then
+        set_agi_variable "CALLERID(num)" "$final_cid"
+    fi
+
+    exit 0
+}
+
+main "$@"
